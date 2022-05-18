9c514b89c5d7dd470b548f82fa8100af22420ed1
sitogi <takano162@gmail.com>
2022-05-19 00:09:27 +0900

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (<div className="flex flex-col items-center justify-center min-h-screen bg-black text-white text-3xl">
+    hello world!
+  </div>
+  )
+}
+
+export default App
